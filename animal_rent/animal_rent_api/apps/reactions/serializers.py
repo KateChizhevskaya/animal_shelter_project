95ffb42d11744b89f2e97a418f1aa9881f419aac
KateChizhevskaya <Katarinachig16@gmail.com>
2020-10-22 10:58:59 +0300

    rewrite reactions

--- animal_rent/animal_rent_api/apps/reactions/serializers.py
+++ animal_rent/animal_rent_api/apps/reactions/serializers.py
@@ -1,10 +1,10 @@
+from django.db import connection
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 from animal_rent_api.apps.animals.models import Animal
 from animal_rent_api.apps.reactions.models import Review, Complaint
 from animal_rent_api.apps.requests.constants import Statuses
-from animal_rent_api.apps.requests.models import RentRequest
 from animal_rent_api.apps.user.models import RentUser
 
 
@@ -62,11 +62,15 @@ class AddReviewSerializer(ModelSerializer):
 	def _validate_animal(self, attr):
 		animal = attr.get('animal')
 		user = self.context['request'].user
-		if not RentRequest.objects.filter(
-			status=Statuses.APPROVED,
-			animal=animal,
-			renter=user
-		).exists():
+		with connection.cursor() as cursor:
+			cursor.execute(
+				'''SELECT * from apps_rentrequest where status = %s AND animal_id = %s AND renter_id = %s''',
+				(
+					Statuses.APPROVED, animal.id, user.id
+				)
+			)
+			existed_rent_request = cursor.fetchone()
+		if not existed_rent_request:
 			raise serializers.ValidationError(
 				'You can not add comment to animal, that you have not rent yet'
 			)
@@ -84,12 +88,21 @@ class AddReviewSerializer(ModelSerializer):
 
 	def recalculate_animal_raiting(self, instance):
 		animal = instance.animal
-		animal_reviews = list(filter(
-			lambda rating: rating is not None,
-			Review.objects.filter(animal=animal).values_list('rating', flat=True)
-		))
-		animal.rating = sum(map(lambda str_num: int(str_num), animal_reviews)) / len(animal_reviews)
-		animal.save(update_fields=('rating', ))
+		with connection.cursor() as cursor:
+			cursor.execute(
+				'''SELECT rating from apps_review where animal_id = %s''',
+				(
+					animal.id,
+				)
+			)
+			animal_reviews = [item[0] for item in cursor.fetchall()]
+			new_rating = sum(map(lambda str_num: int(str_num), animal_reviews)) / len(animal_reviews)
+			cursor.execute(
+					'''UPDATE apps_animal SET rating = %s WHERE id = %s''',
+					(
+						new_rating, animal.id
+					)
+				)
 
 	def create(self, validated_data):
 		instance = super(AddReviewSerializer, self).create(validated_data)
